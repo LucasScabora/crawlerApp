bcc7f8a7d867dc2d8b039168cd82d6a9726dcae1
Lucas de Carvalho Scabora <csb.lucas@gmail.com>
2020-09-24 21:59:43 -0300

    Organizing DB manager class

---
+++ crawlerApp/crawlerApp/dbManager.py
@@ -0,0 +1,131 @@
+# General imports
+import os
+import hashlib
+import sqlite3 as sl
+
+from urllib.parse import urlparse
+
+
+# Exception when managing the database
+class dbException(Exception):
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message    = message
+
+
+# Define database manipulations
+class dbManager:
+    
+    # Initializes database
+    def __init__(self, name = 'database_urls.db'):
+        # Validates database name
+        if len(name) < 1:
+            raise dbException('Database Name', 'Name is too short. Minimum of 1 character!')
+
+        # Creates URL table if the database file does not exists
+        will_create_table = not (os.path.isfile(name))
+        
+        self.flagConnOpen = False
+        try:
+            self.conn         = sl.connect(name)
+            self.flagConnOpen = True
+
+            if will_create_table:
+                self.createSchema()
+
+        except Exception as ex:
+            raise dbException('Error creating database', ex)
+
+
+    # Create the URL table
+    def createSchema(self):
+        self.conn.execute("""
+            CREATE TABLE URLs (
+                id            INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                url           TEXT,
+                acquired_date DATE,
+                url_hash      TEXT
+            );""")
+
+        # Creates a index to validate similar URLs
+        self.conn.execute("""CREATE INDEX SIMILAR_URLs ON URLs(url_hash);""")
+
+    # Close database connection
+    def close(self):
+        if( self.flagConnOpen ):
+            self.conn.close()
+            self.flagConnOpen = False
+
+    # List tuples in URL table
+    def listURLs(self):
+        if( self.flagConnOpen ):
+            try:
+                cur = self.conn.cursor()
+
+                # Fetches and print all rows of URL table
+                cur.execute("SELECT acquired_date, url FROM URLs")
+                print('The URL table have the following URL links:')
+                for row in cur.fetchall():
+                    print(row)
+
+            except Exception as ex:
+                raise dbException('Error listing URLs', ex)
+        else:
+            print('Unable to print URLs because database is closed')
+
+
+    # Saves a new URL, returning its id
+    def appendURL(self, url, acquired_date):
+        if( self.flagConnOpen ):
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("""INSERT INTO URLs (url, acquired_date, url_hash) 
+                                  VALUES ('{}', '{}', '{}')""".format(url, 
+                                  	acquired_date, self.hashURL(url)))
+                self.conn.commit()
+
+                # Returns the URL's id
+                last_id = int(cursor.lastrowid)
+                cursor.close()
+                return last_id
+
+            except Exception as ex:
+                raise dbException('Error saving URL', ex)
+        else:
+            print('Unable to print URLs because database is closed')
+            return -1
+
+
+    # Validates duplicated URLs
+    def isDublicatedURL(self, url):
+        if( self.flagConnOpen ):
+            # Hashes the URL
+            hash_url = self.hashURL(url)
+
+            # Searches for duplicated URLs based on the hash value
+            cursor = self.conn.cursor()
+            cursor.execute("""SELECT id, url FROM URLs WHERE url_hash = '{}'""".format(hash_url))
+            for row in cursor.fetchall():
+                if (row[1] == url):
+                    return True
+
+            # Not duplicated
+            return False
+
+
+    # General URL manipulations
+    # Returns escaped URL (for storing in the database)
+    def escapeURL(self, url):
+        return url.replace("'","''")
+
+    # Validates URL. Returns True if the URL is valid, otherwise returns False
+    def isValidURL(self, url):
+        parsed_url = urlparse(url)
+        if bool(parsed_url.scheme): 
+            return True
+        else:
+            return False
+
+    # Get MD5 from URL string
+    def hashURL(self, url):
+        return hashlib.md5(url.encode('utf-8')).hexdigest()
--- crawlerApp/crawlerApp/pipelines.py
+++ crawlerApp/crawlerApp/pipelines.py
@@ -1,94 +1,31 @@
 # Define your item pipelines here
 
-import os
-import hashlib
-import sqlite3 as sl
+from itemadapter          import ItemAdapter
+from scrapy.exceptions    import DropItem
+from scrapy.exceptions    import DropItem
 
-from urllib.parse import urlparse
-from itemadapter import ItemAdapter
-from scrapy.exceptions import DropItem
-
+from crawlerApp.dbManager import dbManager
 
 
 class CrawlerappPipeline:	
-    # Opens a database connection (to do)
+    # Starts the spider of the crawler
     def open_spider(self, spider):
-        # Flag to determine if a table need to be created
-        will_create_table = False
-
-        # Checks if a database file does not exists
-        if ( os.path.isfile('database_urls.db') == False ):
-            will_create_table = True
-
-        # Connects to the database (or create a new one)
-        try:
-            # Creates the connection
-            self.conn = sl.connect('database_urls.db')
-
-            # If a new database is created, also creates the Table
-            if (will_create_table == True):
-                self.conn.execute("""
-                    CREATE TABLE URLs (
-                        id            INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                        url           TEXT,
-                        acquired_date DATE,
-                        url_hash      TEXT
-                    );""")
-
-                # Also, creates a index to validate similar URLs
-                self.conn.execute("""CREATE INDEX SIMILAR_URLs ON URLs(url_hash);""")
-        except Error as ex:
-            print('Error creating database: ' + ex)
-            print('\nThe crawler will proceed without storing the URLs')
+        self.db = dbManager('database_urls.db')
        
-    # List results and closes a database connection
+
+    # Finishes the spider of the crawler
     def close_spider(self, spider):
-        # List all URLs stored in the database
-        try:
-            cur = self.conn.cursor()
+        self.db.listURLs()
+        self.db.close()
 
-            # Fetches all the rows
-            cur.execute("SELECT acquired_date, url FROM URLs")
-            rows = cur.fetchall()
-
-            # Iterates over the rows printing them
-            print('The URL table have the following URL links:')
-            for row in rows:
-                print(row)
-
-            # Finally, closes the connection
-            self.conn.close()
-        except Exception as ex:
-            print('Error in the database, URLs might be compromised: ' + ex)
 
     # Process each URL found
     def process_item(self, item, spider):
-        # Cleans the url (escapes single quotes)
-        clean_url = item['link'].replace("'","''")
+        # Escape quotes in the URL
+        escaped_url = self.db.escapeURL(item['link'])
 
-        # Validates the URL if it has a scheme
-        parsed_url = urlparse(clean_url)
-        if bool(parsed_url.scheme): 
-            # Hashes URL to validate duplicated elements (Uses MD5 but can be others)
-            hash_url = hashlib.md5(clean_url.encode('utf-8')).hexdigest()
-
-            # Searches for duplicated URLs based on the hash values
-            cur = self.conn.cursor()
-            cur.execute("""SELECT id, url FROM URLs WHERE url_hash = '{}'""".format(hash_url))
-            rows = cur.fetchall()
-            for row in rows:
-                if (row[1] == clean_url):
-                    print('[ERROR] Dupplicated URL. In database it is id = ' + str(row[0]))
-                    raise DropItem("""Invalid URL in '{}'""".format(item))
-
-            # Saves (or at least tries to save) the URL into the database
-            try:
-                self.conn.execute("""INSERT INTO URLs (url, acquired_date, url_hash) 
-                                     VALUES ('{}', '{}', '{}')""".format(item['link'], item['date'], hash_url))
-                self.conn.commit()
-            except Error as ex:
-                print('Error saving URL to database: ' + ex)
-            finally:
-                return item
+        if (self.db.isValidURL(escaped_url)) & (not self.db.isDublicatedURL(escaped_url)): 
+            self.db.appendURL(escaped_url, item['date'])
+            return item
         else:
-            raise DropItem("""Invalid URL in '{}'""".format(item))
+            raise DropItem("""Invalid or duplicated URL in '{}'""".format(item))
